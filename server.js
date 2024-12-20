40c2279df2b382bd51ffbd68b09c13108502bc6e
anvar-haseeb <anvar-haseeb@kottsoftware.com>
2024-12-20 12:02:09 +0530

    car-api

--- server.js
+++ server.js
@@ -1,15 +1,15 @@
 const express = require("express");
 const connectDB = require("./db/connectDB");
 const carsRoutes = require("./routes/carRoutes");
-const cors = require('cors'); 
+const cors = require("cors");
 const errorHandler = require("./middleware/errorhandlers");
-require("express-async-error")
+require("express-async-error");
 
 const app = express();
 const PORT = process.env.PORT || 3005;
 
 app.use(cors()); //to cross-origin resource sharing
-app.use(express.json())// to accept json data
+app.use(express.json()); // to accept json data
 
 app.use("/cars", carsRoutes);
 
@@ -17,7 +17,7 @@ app.get("*", (req, res) => {
   res.status(404).json({ message: "Invalid route" });
 });
 
-app.use(errorHandler)
+app.use(errorHandler);
 connectDB().then(() => {
   app.listen(PORT, () => console.log(`SERVER RUNNING ON ${PORT}`));
 });
